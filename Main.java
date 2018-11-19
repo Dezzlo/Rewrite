07b25f928f59a94c4a9f49adff7fa4e64a754734
PryTonick <42908445+Dezzlo@users.noreply.github.com>
2018-11-19 08:37:34 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        Thread copy_Text = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    BufferedReader bufferedReader = new BufferedReader
+                            (new FileReader("C:\\Programs Gorshka\\Programming\\IntelliJ IDEA\\" +
+                                    "Java coding\\Rewrite\\src\\Text_File.txt"));
+
+                    String string;
+                    while ((string = bufferedReader.readLine()) != null) {
+                        arrayList.add(string);
+                    }
+
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+        };
+        copy_Text.start();
+        Thread rewrite = new Thread() {
+
+            @Override
+            public void run() {
+                try{
+                    FileWriter writer = new FileWriter("File_Without_Text.txt");
+
+                    for(String line : arrayList)
+                    {
+                        writer.write(line);
+                        writer.write(System.getProperty("line.separator"));
+
+                    }
+
+                    writer.close();
+                }catch (IOException ex){ex.printStackTrace();}
+            }
+        };
+        rewrite.start();
+    }
+}
